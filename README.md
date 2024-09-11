f812c7890a5abd221c779079f2182d1449f94439
NileyGF <nileygf@gmail.com>
2024-09-11 14:44:10 -0400

    basic project structure

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hintsly
+
+AI functionalities for the Hintsly project.
+
+## Installation
+
+This guide assumes you have Python version >= 3.9 installed.
+
+### Create a virtual environment
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
---
+++ scr/settings.py
@@ -0,0 +1,6 @@
+import os
+
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))   # Hintsly/src
+BASE_DIR = os.path.dirname(SRC_DIR)                    # Hintsly
+TEST_DIR = os.path.join(BASE_DIR, 'test')              # Hintsly/test
+DATA_DIR = os.path.dirname(SRC_DIR)                    # Hintsly/data
